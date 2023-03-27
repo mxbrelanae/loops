f72b4f7e0d57412d5d28eaf57a843397b4820f4e
Bre Dale <bredale122@gmail.com>
2023-03-27 14:42:01 -0400

    practice loops

---
+++ loops.js
@@ -0,0 +1,23 @@
+for(let i = 0; i <= 10; i++ ){
+   console.log(i);
+}
+
+let j = 0;
+while(j <= 10){
+    console.log(j);
+    j++;
+}
+
+for(let k = 0; k <= 20; k+=2 ){
+    console.log(k);
+}
+
+let l = 1;
+while(l <= 20){
+    console.log(l);
+    l+=2;
+}
+
+for(let m = 10; m >= 0; m--){
+    console.log(m);
+}
